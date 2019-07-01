166bfa300a7923cff32079fcc8b195340ae9b78d
christian0106 <christiancontreras7b12@gmail.com>
2019-07-01 11:28:59 -0600

    avance

---
+++ ParcialFinal/ParcialFinal/src/parcialfinal/ParcialFinal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialfinal;
+
+import views.Menu;
+
+/**
+ *
+ * @author hugo
+ */
+public class ParcialFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu men = new Menu();
+        men.setTitle("Menu principal");
+        men.setVisible(true);
+    }
+    
+}
